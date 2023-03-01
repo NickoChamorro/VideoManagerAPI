1852f11f9af3a343823bc695e5b74fe109fa5551
Nicolás Chamorro <nicko.chamorro@gmail.com>
2023-03-01 01:26:02 -0300

    update upload function

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const uploadVideo = async (req, res)=>{
         console.log(`entra al try`);
         
         /* console.log(`name ${fileVideo}`); */
-        console.log(`name ${storage._handleFile.arguments}`);
+        console.log(`fileVideo.fieldname ${fileVideo.fieldname} - fileVideo.originalname ${fileVideo.originalname}`);
 
         const nameVideo = 'video_'+ new Date();
         // Upload
